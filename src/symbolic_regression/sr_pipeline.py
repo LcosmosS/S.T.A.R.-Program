3bbfb0eca3c9c158d03f2b02f20cb1af1e800890
Patrick J. McNamara <pmqr771@gmail.com>
2026-04-29 22:56:19 -0400

    Update sr_pipeline.py

--- src/symbolic_regression/sr_pipeline.py
+++ src/symbolic_regression/sr_pipeline.py
@@ -15,7 +15,12 @@ This module:
 import numpy as np
 from .constrained_gp import ConstrainedGP
 from .law_discovery_manifold import LawDiscoveryManifold
+from src.data.load_sky_surveys import load_sky_surveys
 
+def test_sky_surveys_load():
+    df1, df2 = load_sky_surveys(downsample=100, validate_schema=True)
+    assert len(df1) > 0
+    assert len(df2) > 0
 
 class SRPipeline:
     """
