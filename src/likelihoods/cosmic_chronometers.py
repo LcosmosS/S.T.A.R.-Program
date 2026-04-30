72d44f8737de48c170b96ab431afac7762ca1564
Patrick J. McNamara <pmqr771@gmail.com>
2026-04-30 05:23:02 -0400

    Update cosmic_chronometers.py

--- src/likelihoods/cosmic_chronometers.py
+++ src/likelihoods/cosmic_chronometers.py
@@ -11,14 +11,21 @@ import pandas as pd
 
 
 class CosmicChronometers:
-    def __init__(self, cc_csv):
-        self.cc = pd.read_csv(cc_csv)
+    def __init__(self, cc_data):
+        """
+        cc_data can be:
+        - a dict (embedded data module)
+        - a CSV file path (legacy mode)
+        """
+        if isinstance(cc_data, dict):
+            # Embedded data mode
+            self.cc = cc_data
+        else:
+            # Legacy CSV mode
+            df = pd.read_csv(cc_data)
+            self.cc = {
+                "z": df["z"].tolist(),
+                "H": df["H"].tolist(),
+                "sigma_H": df["sigma_H"].tolist(),
+            }
 
-    def log_likelihood(self, model):
-        z = self.cc["z"].values
-        H_obs = self.cc["H"].values
-        sigma = self.cc["sigma_H"].values
-
-        H_model = np.array([model.H(zi) for zi in z])
-        chi2 = np.sum(((H_obs - H_model) / sigma)**2)
-        return -0.5 * chi2
