52d0f89d5a9cd132f7b67f774dcfc10b1598cef6
Patrick J. McNamara <pmqr771@gmail.com>
2026-04-29 23:05:56 -0400

    Update __init__.py

--- src/acsc/__init__.py
+++ src/acsc/__init__.py
@@ -15,6 +15,12 @@ from .projection import project
 from .quantile import QuantileAligner
 from .tda_pipeline import compute_persistence
 from .statistics import w2_between_diagrams, empirical_p_value
+from src.data.load_sky_surveys import load_sky_surveys
+
+def test_sky_surveys_load():
+    df1, df2 = load_sky_surveys(downsample=100, validate_schema=True)
+    assert len(df1) > 0
+    assert len(df2) > 0
 
 __all__ = [
     "project",
