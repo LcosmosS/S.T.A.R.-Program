ea73dc3b0b9c1028bea217b48e0a31554bc75624
Patrick J. McNamara <pmqr771@gmail.com>
2026-04-29 22:58:55 -0400

    Update cosmic_lagrangian.py

--- src/physics/cosmic_lagrangian.py
+++ src/physics/cosmic_lagrangian.py
@@ -15,7 +15,12 @@ import numpy as np
 from ..entropy.entropy_field import EntropyField
 from ..entropy.differential_forms import DifferentialForms
 from ..physics.scalar_coupling import ScalarCoupling
+from src.data.load_sky_surveys import load_sky_surveys
 
+def test_sky_surveys_load():
+    df1, df2 = load_sky_surveys(downsample=100, validate_schema=True)
+    assert len(df1) > 0
+    assert len(df2) > 0
 
 class CosmicLagrangian:
     """
