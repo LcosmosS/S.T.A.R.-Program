44407d41a80ba8201e018078a7ffab63b802912e
Patrick J. McNamara <pmqr771@gmail.com>
2026-04-30 23:00:50 -0400

    Update desi_bao.py

--- src/likelihoods/desi_bao.py
+++ src/likelihoods/desi_bao.py
@@ -20,10 +20,8 @@ class DESIBAO:
         - a CSV file path (legacy mode)
         """
         if isinstance(bao_data, dict):
-            # Embedded data mode
             self.bao = bao_data
         else:
-            # Legacy CSV mode
             df = pd.read_csv(bao_data)
             self.bao = {
                 "z": df["z"].tolist(),
@@ -38,18 +36,33 @@ class DESIBAO:
     def log_likelihood(self, model):
         chi2 = 0.0
 
-    # Embedded dict mode
-    if isinstance(self.bao, dict):
-        z_list = self.bao["z"]
-        DM_over_rd_list = self.bao["DM_over_rd"]
-        sigma_DM_list = self.bao["sigma_DM"]
-        H_rd_list = self.bao["H_rd"]
-        sigma_H_list = self.bao["sigma_H"]
+        # Embedded dict mode
+        if isinstance(self.bao, dict):
+            z_list = self.bao["z"]
+            DM_over_rd_list = self.bao["DM_over_rd"]
+            sigma_DM_list = self.bao["sigma_DM"]
+            H_rd_list = self.bao["H_rd"]
+            sigma_H_list = self.bao["sigma_H"]
 
-        for z, DM_obs, sDM, H_obs, sH in zip(
-            z_list, DM_over_rd_list, sigma_DM_list, H_rd_list, sigma_H_list
-        ):
-            # Model predictions
+            for z, DM_obs, sDM, H_obs, sH in zip(
+                z_list, DM_over_rd_list, sigma_DM_list, H_rd_list, sigma_H_list
+            ):
+                DM_model = model.DM(z) / self.r_d
+                H_model = model.H(z) * self.r_d
+
+                chi2 += ((DM_obs - DM_model) / sDM) ** 2
+                chi2 += ((H_obs - H_model) / sH) ** 2
+
+            return -0.5 * chi2
+
+        # Legacy CSV mode (DataFrame)
+        for _, row in self.bao.iterrows():
+            z = row["z"]
+            DM_obs = row["DM_over_rd"]
+            sDM = row["sigma_DM"]
+            H_obs = row["H_rd"]
+            sH = row["sigma_H"]
+
             DM_model = model.DM(z) / self.r_d
             H_model = model.H(z) * self.r_d
 
@@ -57,19 +70,3 @@ class DESIBAO:
             chi2 += ((H_obs - H_model) / sH) ** 2
 
         return -0.5 * chi2
-
-    # Legacy CSV mode (DataFrame)
-    for _, row in self.bao.iterrows():
-        z = row["z"]
-        DM_obs = row["DM_over_rd"]
-        sDM = row["sigma_DM"]
-        H_obs = row["H_rd"]
-        sH = row["sigma_H"]
-
-        DM_model = model.DM(z) / self.r_d
-        H_model = model.H(z) * self.r_d
-
-        chi2 += ((DM_obs - DM_model) / sDM) ** 2
-        chi2 += ((H_obs - H_model) / sH) ** 2
-
-    return -0.5 * chi2
