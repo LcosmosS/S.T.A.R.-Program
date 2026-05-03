7bfc823a28ae2a87daddb439f5f877e12343be0d
Patrick J. McNamara <pmqr771@gmail.com>
2026-05-02 21:43:01 -0400

    Delete src/likelihoods/data/pantheon_plus.py

--- src/likelihoods/data/pantheon_plus.py
+++
@@ -1,8 +0,0 @@
-# Auto-generated Pantheon+ binned dataset (z, mu, sigma_mu)
-# This is a compact, non-copyright dataset representation.
-
-PANTHEON_PLUS = {
-    "z": [...],
-    "mu": [...],
-    "sigma_mu": [...]
-}
