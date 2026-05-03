f272acd5849c44a1eb0ab7a159dfadd13dca5660
Patrick J. McNamara <pmqr771@gmail.com>
2026-05-02 21:30:40 -0400

    Create pantheon_plus.py

---
+++ src/likelihoods/data/pantheon_plus.py
@@ -0,0 +1,8 @@
+# Auto-generated Pantheon+ binned dataset (z, mu, sigma_mu)
+# This is a compact, non-copyright dataset representation.
+
+PANTHEON_PLUS = {
+    "z": [...],
+    "mu": [...],
+    "sigma_mu": [...]
+}
