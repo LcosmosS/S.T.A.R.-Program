b2b0743e8859e9082bb4cd7c377320b2eb26d682
Patrick J. McNamara <pmqr771@gmail.com>
2026-05-03 00:28:16 -0400

    Create __init__.py

---
+++ src/likelihoods/data/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pantheon Plus package for S.T.A.R. Model.
+
+This file marks the directory as a Python package so modules inside
+can be imported as:
+
+    from src.visualization.paper_figures_pipeline import PaperFiguresPipeline
+"""
